d44ed1c83a26d755ce1bf85ccc2a57aa6c61c6f4
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2022-03-01 02:27:52 -0800

    Initial commit

---
+++ Instagram/Instagram/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  Instagram
+//
+//  Created by Sidhdharth Pandya on 3/1/22.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionlabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
